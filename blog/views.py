2f577baabec0336d76d92b28933e418b66766ebb
Carlos <cpiscoyat@gmail.com>
2016-01-16 17:11:54 -0500

    Modificacion para mostrar datos de la BD

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 #from django.template import loader, Context
 #from django http import HttpResponse
 #from blog.models import Post
@@ -12,4 +14,5 @@ from django.shortcuts import render
 #	return HttpResponse(mi_template.render(mi_contexto))
 
 def post_list(request):
-        return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts': posts})
